974b1f1af2f4b6a8e37ce5a9e113b2f45dc172db
Scott Andrus <scottandrus92@gmail.com>
2014-04-07 22:21:22 -0500

    Initial project commit

---
+++ iPub/IPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPAppDelegate.h
+//  iPub
+//
+//  Created by Scott Andrus on 4/7/14.
+//  Copyright (c) 2014 Tapatory, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iPub
+//
+//  Created by Scott Andrus on 4/7/14.
+//  Copyright (c) 2014 Tapatory, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IPAppDelegate class]));
+    }
+}
